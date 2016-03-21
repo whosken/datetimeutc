0de10b87114aee7adc93481e11a4f99f0ae521bf
Ken Hu <whosbacon@gmail.com>
2016-03-21 12:50:48 +0900

    init commit, pypi setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# datetimeutc
-Because why are we dealing with timezones?
+#datetimeutc
+
+## Because why are we dealing with timezones?
+
+A lightweight datetime utility library which wraps around the standard datetime module, with features from dateutil, and pytz.
+
+It's purpose is to minimize the effort to convert datetime to and from UTC, a task I found myself doing too often when building web services and social driven applications.
---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(name='datetimeutc',
+                 version='0.2.2.1',
+                 description='Because why are we dealing with timezones?',
+                 url='http://github.com/whosken/datetimeutc',
+                 author='Ken Hu',
+                 author_email='whosbacon@gmail.com',
+                 license='MIT',
+                 packages=['datetimeutc'],
+                 install_requires=['pytz','python-dateutil'],
+                 zip_safe=False)
